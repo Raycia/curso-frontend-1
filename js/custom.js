b94dc7611cbc7762ff4f82ef3b27660cc6ae6079
Raycia <raycialima@gmail.com>
2023-05-21 18:18:27 -0300

    adicionando incrementações de validação

--- js/custom.js
+++ js/custom.js
@@ -1,62 +1,267 @@
 // instancia jquery e evita conflitos
 // jQuery( function($){
 $(document).ready(function(){
-
-    $('.owl-carousel').owlCarousel();
-
-    let titulos = $('h4') // tag
    
-    let itens = $('.featured-item') // class
+   $('.owl-carousel').owlCarousel();
+   
+   let titulos = $('h4') // tag
+   
+   let itens = $('.featured-item') // class
+   
+   let destaques = $('#featured') // id
+   
+   // console.log(titulos.first());
+   
+   // Configuração de produtos
+   
+   $('.featured-item a').addClass('btn btn-dark stretch-link');
+   
+   $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+   // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
+   // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
+   // $('.featured-item:first h4').addClass('active')
+   // $('.featured-item:first h4').removeClass('active')
+   // $('.featured-item:first h4').toggleClass('active')
+   // $('.featured-item:first h4').hide()
+   // $('.featured-item:first h4').show()
+   // $('.featured-item:first h4').fadeIn(2000)
+   // $('.featured-item:first h4').fadeOut()
+   //  $('.featured-item:first h4').css('color', '#f00')
+
+   
+   $('.featured-item h4').dblclick( function(){
+      
+      $(this).css({
+         'color': '#f00',
+         'background': '#ff0',
+         'font-weight': '100',
+      });
+      
+   });
+   
+   /*
+    * Manipulação de eventos
+    */
+   $('.featured-item a').on('blur', function(event){
+      
+      event.preventDefault();
+      
+      alert('Produto esgotado');
+      
+   })
+
+   /* 
+    * Callback
+    * entendendo ações que começam ao termino de outra
+    */
+   $('.featured-item:nth(1)')
+      .hide(500, function(){
+         // este é o callback
+         console.log( $(this).find('h4').text() + ' esgotado')
+      })
+      .show(500, function(){
+         console.log( $(this).find('h4').text() + ' em estoque')
+      })
+ 
+
+   /*
+    * Animações
+    */
+   const duracao = 1000 // equivalenta a 1 segundo
+
+   $('.featured-item:nth(0)')
+      .hide(duracao)
+      .show(duracao)
+      .fadeOut(duracao)
+      .fadeIn(duracao)
+      .toggle(duracao)
+      .toggle(duracao)
+
+   $('#form-submit').on('click', function(e){
+
+      e.preventDefault()
+
+      if( $('#email').val() != '' ){
+
+         $('#email').animate({
+            opacity: "toggle",
+            top: "-50"
+         }, 500, function(){
+            console.log($(this).val())
+         })
+
+      }
+
+
+   });
+
+
+   /*
+    * Ouvinte de eventos .nav-modal-open
+    */
+   $('.nav-modal-open').on('click', function(e){
+
+      e.preventDefault();
+
+      let elem = $(this).attr('rel')
+
+      $('.modal-body').html($('#'+elem).html())
+      
+      $('.modal-header h5.modal-title').html($(this).text())
+
+      let myModal = new bootstrap.Modal($('#modelId'))
+
+      myModal.show()
+
+
+   })
+
+
+   /*
+    * TODO: incrementar a validação
+    * - checar se o nome é válido (mais de 2 caracteres)
+    * - checar se o email é válido com ao menos um "@" e "."
+    * - checar se o cpf é válido com regex
+    */
+   function validate( elem ){
+      if( elem.val() == '') {
+
+         console.log('o campo de '+ elem.attr('name') + ' é obrigatório')
+
+         elem.parent().find('.text-muted').show()
+
+         elem.addClass('invalid')
+
+         return false
+      } else {
+         elem.parent().find('.text-muted').hide()
+         elem.removeClass('invalid')
+      }
+   }
+
+   function validateName(elem) {
+      const name = elem.val().trim();
+      if (name.length < 3) {
+        console.log('O campo de nome deve ter mais de 2 caracteres');
+        elem.parent().find('.text-muted').show();
+        elem.addClass('invalid');
+        return false;
+      } else {
+        elem.parent().find('.text-muted').hide();
+        elem.removeClass('invalid');
+      }
+    }
+
+
+    function validateEmail(elem) {
+      const email = elem.val().trim();
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        console.log('O campo de email deve conter um endereço de email válido');
+        elem.parent().find('.text-muted').show();
+        elem.addClass('invalid');
+        return false;
+      } else {
+        elem.parent().find('.text-muted').hide();
+        elem.removeClass('invalid');
+      }
+    }
+
+
+    function validateCPF(elem) {
+      const cpf = elem.val().replace(/[^\d]+/g, '');
+      if (
+        cpf.length !== 11 ||
+        cpf === '00000000000' ||
+        cpf === '11111111111' ||
+        cpf === '22222222222' ||
+        cpf === '33333333333' ||
+        cpf === '44444444444' ||
+        cpf === '55555555555' ||
+        cpf === '66666666666' ||
+        cpf === '77777777777' ||
+        cpf === '88888888888' ||
+        cpf === '99999999999'
+      ) {
+        console.log('O campo de CPF deve conter um CPF válido');
+        elem.parent().find('.text-muted').show();
+        elem.addClass('invalid');
+        return false;
+      } else {
+        elem.parent().find('.text-muted').hide();
+        elem.removeClass('invalid');
+      }
+    }
     
-    let destaques = $('#featured') // id
+    
 
-    console.log(titulos.first());
+   $('body').on('submit', '.modal-body .form', function(e){
 
-    // Configuração de produtos
+      e.preventDefault()
 
-    $('.featured-item a').addClass('btn btn-dark stretch-link');
+      const inputName = $('#nome')
+      const inputEmail = $('#email')
 
-    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').addClass('active')
-    // $('.featured-item:first h4').removeClass('active')
-    // $('.featured-item:first h4').toggleClass('active')
-    // $('.featured-item:first h4').hide()
-    // $('.featured-item:first h4').show()
-    // $('.featured-item:first h4').fadeIn(2000)
-    // $('.featured-item:first h4').fadeOut()
-    //  $('.featured-item:first h4').css('color', '#f00')
-     
-     $('.featured-item h4').dblclick( function(){
+      validate(inputName)
+      validate(inputEmail)
 
-        $(this).css({
-            'color': '#f00',
-            'background': '#ff0',
-            'font-weight': '100',
-        });
+      if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+         console.log('verificar campos obrigatórios')
+         return false
+      } else {
+         $(this).submit()  
+      }
 
-     });
+   })
 
-     /*
-      * Manipulação de eventos
-      */
-     $('.featured-item a').on('blur', function(event){
+   $('body').on('blur', '#nome', function(){
+      validate($(this))
+   })
 
-        event.preventDefault();
+   $('body').on('blur', '#nome', function () {
+      validateName($(this));
+    });
 
-        alert('Produto esgotado');
+   $('body').on('blur', '#email', function(){
+      validate($(this))
+   })
 
-     })
-     
-     
-     
-     
+   $('body').on('blur', '#email', function () {
+      validateEmail($(this));
+    });
+ 
+   $('body').on('focus', '#date', function(){
+      $(this).datepicker()
+   })
 
+   $('body').on('blur', '#date', function(){
+      validate($(this))
+      $(this).mask('00/00/0000');
+   }) 
 
+   $('body').on('blur', '#time', function(){
+      validate($(this))
+      $(this).mask('00:00');
+   })
 
+   $('body').on('blur', '#cep', function(){
+      validate($(this))
+      $(this).mask('00000-000');
+   })
 
+   $('body').on('blur', '#phone', function(){
+      validate($(this))
+      $(this).mask('00000-0000');
+   })
 
+   $('body').on('blur', '#cpf', function(){
+      validate($(this))
+      $(this).mask('000.000.000-00');
+   })
 
+   $('body').on('blur', '#cpf', function () {
+      validateCPF($(this));
+    });
+    
 
 })
